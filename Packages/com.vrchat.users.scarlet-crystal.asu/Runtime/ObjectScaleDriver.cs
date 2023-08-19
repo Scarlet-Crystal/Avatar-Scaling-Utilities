2ff9206eb5a1708526dff8cceaf9aab31c15ba4d
Scarlet-Crystal <141389226+Scarlet-Crystal@users.noreply.github.com>
2023-08-19 00:20:41 -0600

    Make ObjectScaleDriver adjustable at runtime

--- Packages/com.vrchat.users.scarlet-crystal.asu/Runtime/ObjectScaleDriver.cs
+++ Packages/com.vrchat.users.scarlet-crystal.asu/Runtime/ObjectScaleDriver.cs
@@ -9,13 +9,57 @@ namespace AvatarScalingUtilities
     public class ObjectScaleDriver : UdonSharpBehaviour
     {
         [SerializeField]
-        private float scaleMultiplier = 1f;
+        private float baseEyeHeight = 1.5f, maxEyeHeight = 5f, minEyeHeight = 0.2f;
+
+        public float BaseEyeHeight
+        {
+            get => baseEyeHeight;
+            set
+            {
+                baseEyeHeight = value;
+                RecalculateScale(Networking.LocalPlayer);
+            }
+        }
+
+        public float MaxEyeHeight
+        {
+            get => maxEyeHeight;
+            set
+            {
+                maxEyeHeight = value;
+                RecalculateScale(Networking.LocalPlayer);
+            }
+        }
+
+        public float MinEyeHeight
+        {
+            get => minEyeHeight;
+            set
+            {
+                minEyeHeight = value;
+                RecalculateScale(Networking.LocalPlayer);
+            }
+        }
 
         public override void OnAvatarEyeHeightChanged(VRCPlayerApi player, float prevEyeHeightAsMeters)
         {
             if (player.isLocal)
             {
-                transform.localScale = Vector3.one * (player.GetAvatarEyeHeightAsMeters() * scaleMultiplier);
+                RecalculateScale(player);
+            }
+        }
+
+        void OnEnable()
+        {
+            RecalculateScale(Networking.LocalPlayer);
+        }
+
+        private void RecalculateScale(VRCPlayerApi localPlayer)
+        {
+            if (Utilities.IsValid(localPlayer))
+            {
+                float scale = Mathf.Clamp(localPlayer.GetAvatarEyeHeightAsMeters(), minEyeHeight, maxEyeHeight) / baseEyeHeight;
+                transform.localScale = Vector3.one * scale;
             }
         }
     }
